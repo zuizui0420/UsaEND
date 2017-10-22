7aed420c6ffa2fa042ef2ac3a0888adca0f70f81
shimitaku <nkc.takumi.shouzui@gmail.com>
2017-10-23 06:53:00 +0900

    プレイヤー設定中

---
+++ Assets/Scripts/PhotonManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonManager : Photon.MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//ロビー接続
+	void ConnectPhoton()
+	{
+		PhotonNetwork.ConnectUsingSettings("v1.0");
+	}
+
+	//ロビーに入った時に呼び出される
+	void OnJoinedLobby()
+	{
+		Debug.Log("PhotonManager OnJoinedLobby");
+	}
+
+	//ルーム一覧が取れると
+	void OnReceivedRoomListUpdate()
+	{
+		//ルーム一覧を取る
+		RoomInfo[] rooms = PhotonNetwork.GetRoomList();
+		if (rooms.Length == 0)
+		{
+			Debug.Log("ルームが一つもありません");
+		}
+		else
+		{
+			//ルームが1件以上ある時ループでRoomInfo情報をログ出力
+			for (int i = 0; i < rooms.Length; i++)
+			{
+				Debug.Log("RoomName:" + rooms[i].name);
+				Debug.Log("userName:" + rooms[i].customProperties["userName"]);
+				Debug.Log("userId:" + rooms[i].customProperties["userId"]);
+				GameObject.Find("StatusText").GetComponent<Text>().text = rooms[i].name;
+			}
+		}
+	}
+
+		//ルーム作成
+		public void CreateRoom()
+	{
+		string userName = "ユーザ1";
+		string userId = "user1";
+		PhotonNetwork.autoCleanUpPlayerObjects = false;
+		//カスタムプロパティ
+		ExitGames.Client.Photon.Hashtable customProp = new ExitGames.Client.Photon.Hashtable();
+		customProp.Add("userName", userName); //ユーザ名
+		customProp.Add("userId", userId); //ユーザID
+		PhotonNetwork.SetPlayerCustomProperties(customProp);
+
+		RoomOptions roomOptions = new RoomOptions();
+		roomOptions.customRoomProperties = customProp;
+		//ロビーで見えるルーム情報としてカスタムプロパティのuserName,userIdを使いますよという宣言
+		roomOptions.customRoomPropertiesForLobby = new string[] { "userName", "userId" };
+		roomOptions.maxPlayers = 2; //部屋の最大人数
+		roomOptions.isOpen = true; //入室許可する
+		roomOptions.isVisible = true; //ロビーから見えるようにする
+									  //userIdが名前のルームがなければ作って入室、あれば普通に入室する。
+		PhotonNetwork.JoinOrCreateRoom(userId, roomOptions, null);
+	}
+
+	//ルームに入室した時に呼び出される
+	void OnJoinedRoom()
+	{
+		Debug.Log("PhotonManager OnJoinedRoom");
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,68 +5,118 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 	[SerializeField]
-	private GameObject player;
-	private SpriteRenderer playerSprite;
+	private GameObject player;	
 	[SerializeField]
 	private Animator playerAnimator;
 
+	//private SpriteRenderer playerSprite;
+
 	private Vector2 playerPos;
 	private Quaternion playerRot;
+	private int animaNom;
+	private bool specialAction = false;
+	private float animationTime;
+	private bool isGround = true;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find("Player");
-		playerSprite = GetComponent<SpriteRenderer>();
+		//playerSprite = GetComponent<SpriteRenderer>();
 		playerPos = transform.position;
 		playerRot = transform.rotation;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		Move();
+	void Update ()
+	{
+		if(specialAction == false)
+		{
+			Move();			
+		}
+
+		else if(specialAction == true)
+		{
+			animationTime = playerAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+			if(animationTime >= 1.0f)
+			{
+				specialAction = false;
+			}
+		}
+
+		Jump();
+
+		if(isGround == false)
+		{
+			//playerPos.y -= 0.1f;
+		}
+
+		transform.position = playerPos;
 	}
 
+	//プレイヤー移動
 	int Move()
 	{
-		int i = 0;
+		//int i = 0;
 
 		if (Input.GetKey("left"))
 		{
 			playerPos += Vector2.left * 0.05f;
-			i = -1;
+			animaNom = -1;
 		}
 		else if (Input.GetKey("right"))
 		{
 			playerPos += Vector2.right * 0.05f;
-			i = 1;
+			animaNom = 1;
 		}
 
 		else
 		{
-			i = 0;
+			animaNom = 0;
 		}
 
-		MoveAnimation(i);
-
-		transform.position = playerPos;
+		Animation(animaNom);
+	
 		return 0;
 	}
 
-	int MoveAnimation(int i)
+	void Jump()
+	{
+
+		float posY = transform.position.y;
+		
+		if (Input.GetKeyDown("up")&&isGround == true)
+		{
+			specialAction = true;
+			Animation(2);
+			posY = transform.position.y + 3.0f;
+			//playerPos.y += 3;
+		}
+
+		while (playerPos.y <= posY)
+		{
+			playerPos.y = Mathf.Lerp(transform.position.y, posY, 0.1f);
+		}
+		
+	}
+
+	//プレイヤーアニメーション
+	int Animation(int i)
 	{
 		switch (i)
 		{
-
+			//止まっている
 			case 0:
 				playerAnimator.Play("playerStop");
 
 				break;
-			case -1:
+            //右移動
+			case 1:
 				playerAnimator.Play("playerMove");
 				playerRot.y = -180;
 
 				break;
-			case 1:
+            //左移動
+			case -1:
 				playerAnimator.Play("playerMove");
 				if(playerRot.y < 0)
 				{
@@ -74,9 +124,35 @@ public class Player : MonoBehaviour {
 				}
 					
 				break;
+
+            //ジャンプ
+			case 2:
+				playerAnimator.Play("playerJump");
+				
+
+				break;
 			
-		};transform.rotation = playerRot;
+		};
+
+		transform.rotation = playerRot;
 		
 		return 0;
 	}
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (collision.gameObject.name == "Stage")
+		{
+			isGround = true;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.gameObject.name == "Stage")
+		{
+			isGround = false;
+		}
+	}
+
 }
