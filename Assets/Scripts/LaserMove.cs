461fa39f933a5c0e0973edad7de1008a3352f66f
Toshi <tosi.akatuki@gmail.com>
2017-11-30 13:32:49 +0900

    aa

---
+++ Assets/Scripts/LaserMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserMove : MonoBehaviour {
+    public float minpos;
+    public float maxpos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Mathf.Clamp(), transform.position.y, 0);
+	}
+}
