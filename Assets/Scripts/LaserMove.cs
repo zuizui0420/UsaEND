a2b4fdc77a63971f221ff393b2b6acb7973d35b7
Toshi <tosi.akatuki@gmail.com>
2017-12-06 13:35:34 +0900

    henkou

--- Assets/Scripts/LaserMove.cs
+++ Assets/Scripts/LaserMove.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LaserMove : MonoBehaviour {
+public class LaserMove : MonoBehaviour
+{
+    //動く範囲の最小値入力
     public float minpos;
+    //動く範囲の最大値入力
     public float maxpos;
+    private static bool flip;
+    // Use this for initialization
+    void Start()
+    {
+        flip = true;
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.position = new Vector3(Mathf.Clamp(), transform.position.y, 0);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minpos, maxpos), transform.position.y, 0);
+    }
+    private void FixedUpdate()
+    {
+        if (flip){
+            transform.position += new Vector3(0.1f, 0, 0);
+            if (transform.position.x >= maxpos)
+                flip = false;
+        }
+        else{
+            transform.position -= new Vector3(0.1f, 0, 0);
+            if (transform.position.x <= minpos)
+                flip = true;
+        }
+    }
 }
---
+++ Assets/Scripts/ThornMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThornMove : MonoBehaviour {
+    private static bool thorn;
+    //動く範囲の最小値入力
+    public float min;
+    //動く範囲の最大値入力
+    public float max;
+    // Use this for initialization
+    void Start () {
+      
+        thorn = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, min, max), transform.position.y, 0);
+        if (thorn)
+        {
+            transform.position += new Vector3(0.1f, 0, 0);
+            if (transform.position.x >= max)
+                thorn = false;
+        }
+        else
+        {
+            transform.position -= new Vector3(0.5f, 0, 0);
+            if (transform.position.x <= min)
+                thorn = true;
+        }
+	}
+}
