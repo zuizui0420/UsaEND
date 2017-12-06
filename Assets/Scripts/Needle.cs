a1618fe200310f24d933371096aeadf0eaaffb41
Toshi <tosi.akatuki@gmail.com>
2017-12-06 15:01:08 +0900

    henjou

---
+++ Assets/Scripts/Needle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Needle : MonoBehaviour {
+    Vector3 pos;
+	// Use this for initialization
+	void Start () {
+        pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, 2f);
+          Debug.DrawRay(transform.position, transform.right* 2f, Color.green);
+        if (hit)
+        {
+
+        }
+    }
+}
