7264a9ef668d16b36a41d572e21e975be707f5c7
chanmao <nkc.mao.yamazaki@gmail.com>
2018-01-17 15:25:09 +0900

    no message

---
+++ Assets/mao/remaincount.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class remaincount : MonoBehaviour
+{
+
+    int cursor;
+
+    int Dead;
+
+    int num = 3;
+
+    
+
+    /// <summary>
+    /// 数字スプライト配列
+    /// </summary>
+    public Sprite[] numbers = new Sprite[11];
+
+    /// <summary>
+    /// 残機数表示部分配列
+    /// </summary>
+    public SpriteRenderer[] remainRend = new SpriteRenderer[4];
+
+    public int Digit(int num)
+    {
+        int digit = 1;
+        for (int i = num; i >= 10; i /= 10)
+        {
+            digit++;
+        }
+        return digit;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //正端君のスクリプト読み込み部分
+
+
+
+        
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
