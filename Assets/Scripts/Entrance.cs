9ab85dbfac50a140fdfea64b68b2e631237e4bc7
chanmao <nkc.mao.yamazaki@gmail.com>
2018-01-19 17:09:35 +0900

    貰ったやつ

---
+++ Assets/Scripts/Entrance.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Entrance : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			// シーン遷移コルーチン開始
+			StartCoroutine(GoToSceneCoroutine());
+		}
+
+	}
+
+	private static IEnumerator GoToSceneCoroutine()
+	{
+		// 5秒間待つ
+		yield return new WaitForSeconds(0.5f);
+		// TitleSceneに遷移
+		SceneManager.LoadScene("StageSelectScene");
+	}
+}
---
+++ Assets/StageSelect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class StageSelect : MonoBehaviour {
+
+    public void OnStargeButtanClicke()
+    {
+        Application.LoadLevel("StageMao");
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
