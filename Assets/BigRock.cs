dd6d481b8912dcc57e1e0afab2af729c04e5d02d
chanmao <nkc.mao.yamazaki@gmail.com>
2018-01-19 15:19:23 +0900

    1/19 岩

---
+++ Assets/BigRock.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigRock : MonoBehaviour {
+
+    public bool houkou;
+
+    public bool hansya;
+
+    bool WallHit = false;
+
+    public float speed;
+
+   
+    
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartCoroutine("hukusei");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        if (WallHit == false)
+        {
+            rigidbody.velocity = Vector2.right * speed;
+        }
+        else
+        {
+            rigidbody.velocity = Vector2.left * speed;
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            if (hansya)
+            {
+                WallHit = true;
+            }
+            else
+            {
+                Debug.Log("hit");
+                Destroy(this.gameObject);
+
+            }
+
+        }
+    }
+
+    private IEnumerator hukusei()
+    {
+        // ログ出力  
+        Debug.Log("1");
+
+        // 1秒待つ  
+        yield return new WaitForSeconds(3.0f);
+
+        // ログ出力  
+        Debug.Log("2");
+
+
+    }
+}
--- Assets/mao/remaincount.cs
+++ Assets/mao/remaincount.cs
@@ -10,9 +10,21 @@ public class remaincount : MonoBehaviour
 
     int Dead;
 
-    int num = 3;
+    int zero = 0;
 
-    
+    int digit1;
+    int digit10;
+    int digit100;
+
+    /// <summary>
+    /// 残機
+    /// </summary>
+     public int DeadCount = 0;
+
+    /// <summary>
+    /// 桁数の合計
+    /// </summary>
+    int DigitNum;
 
     /// <summary>
     /// 数字スプライト配列
@@ -22,12 +34,17 @@ public class remaincount : MonoBehaviour
     /// <summary>
     /// 残機数表示部分配列
     /// </summary>
-    public SpriteRenderer[] remainRend = new SpriteRenderer[4];
+    public GameObject[] remainRend = new GameObject[4];
 
-    public int Digit(int num)
+    /// <summary>
+    /// 桁数
+    /// </summary>
+    /// <param name="deadcount"></param>
+    /// <returns></returns>
+    public int Digit(int deadcount)
     {
         int digit = 1;
-        for (int i = num; i >= 10; i /= 10)
+        for (int i = deadcount; i >= 10; i /= 10)
         {
             digit++;
         }
@@ -42,13 +59,63 @@ public class remaincount : MonoBehaviour
 
 
 
-        
- 
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        DigitNum = Digit(DeadCount);
+
+        switch (DigitNum)
+        {
+            case 0:
+
+                break;
+            case 1:
+                if (DeadCount != 0)
+                {
+                    remainRend[1].GetComponent<SpriteRenderer>().enabled = true;
+                    remainRend[1].GetComponent<SpriteRenderer>().sprite = numbers[10];
+                    digit1 = DeadCount;
+
+                    remainRend[0].GetComponent<SpriteRenderer>().sprite = numbers[digit1];
+                } 
+                break;
+            case 2:
+                remainRend[2].GetComponent<SpriteRenderer>().enabled = true;
+                remainRend[2].GetComponent<SpriteRenderer>().sprite = numbers[10];
+                digit10 = DeadCount / 10;
+                digit1 = DeadCount % 10;
+
+                remainRend[0].GetComponent<SpriteRenderer>().sprite = numbers[digit1];
+                remainRend[1].GetComponent<SpriteRenderer>().sprite = numbers[digit10];
+
+                break;
+            case 3:
+                remainRend[3].GetComponent<SpriteRenderer>().enabled = true;
+                remainRend[3].GetComponent<SpriteRenderer>().sprite = numbers[10];
+                digit100 = DeadCount / 100;
+                int w = DeadCount % 100;
+                digit10 = w / 10;
+                digit1 = w % 10;
+
+                remainRend[0].GetComponent<SpriteRenderer>().sprite = numbers[digit1];
+                remainRend[1].GetComponent<SpriteRenderer>().sprite = numbers[digit10];
+                remainRend[2].GetComponent<SpriteRenderer>().sprite = numbers[digit100];
+
+                break;
+
+            case 4:
+                remainRend[0].GetComponent<SpriteRenderer>().sprite = numbers[9];
+                remainRend[1].GetComponent<SpriteRenderer>().sprite = numbers[9];
+                remainRend[2].GetComponent<SpriteRenderer>().sprite = numbers[9];
+                remainRend[3].GetComponent<SpriteRenderer>().sprite = numbers[10];
+                break;
+
+        }
 
     }
 }
